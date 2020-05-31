eb353243e07e547d02d3b8c272ee1749056f0d05
Gary Maccabe <39347848+Maccabe1@users.noreply.github.com>
2020-05-31 20:56:18 +0100

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -8,12 +8,10 @@ public class FizzBuzz {
         if(number == 0) {
             return "0";
         }
-
         if(number % 3 == 0) {
             fb += "Fizz";
         }
-
-        if (number % 5 == 0) {
+        if(number % 5 == 0) {
             fb += "Buzz";
         }
         return fb.isEmpty() ? String.valueOf(number) : fb;
