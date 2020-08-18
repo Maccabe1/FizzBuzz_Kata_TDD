ce39533b4eba13c242350d40a4c078a6e7a8df04
Maccabe1 <gdmaccabe@hotmail.co.uk>
2020-08-18 06:58:24 +0300

    Add first test for 0 values

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,45 +1,13 @@
 package test.java;
 
-import main.java.FizzBuzz;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class FizzBuzzTest {
+public class FizzBuzzTest() {
 
     @Test
-    public void zeroEqualsZero() {
-        assertThat(FizzBuzz.returnOutput(0), is("0"));
-    }
-
-    @Test
-    public void oneEqualsZero() {
-        assertThat(FizzBuzz.returnOutput(1), is("1"));
-    }
-
-    @Test
-    public void threeEqualsFizz() {
-        assertThat(FizzBuzz.returnOutput(3), is("Fizz"));
-    }
-
-    @Test
-    public void fiveEqualsBuzz() {
-        assertThat(FizzBuzz.returnOutput(5), is("Buzz"));
-    }
-
-    @Test
-    public void sixEqualsFizz() {
-        assertThat(FizzBuzz.returnOutput(6), is("Fizz"));
-    }
-
-    @Test
-    public void tenEqualsBuzz() {
-        assertThat(FizzBuzz.returnOutput(10), is("Buzz"));
-    }
-
-    @Test
-    public void fifteenEqualsFizzBuzz() {
-        assertThat(FizzBuzz.returnOutput(15), is("FizzBuzz"));
+    public static void zeroEqualsZero() {
+        assertThat(FizzBuzz.returnOutput(0), is(0));
     }
 }
