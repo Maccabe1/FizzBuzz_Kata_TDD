cfa725610de577120026789d15dcef7c4cc8cb16
Maccabe1 <gdmaccabe@hotmail.co.uk>
2020-08-20 23:12:54 +0300

    Pass 3 equals Fizz test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,9 @@ package main.java;
 
 public class FizzBuzz {
     public static String returnOutput(int i) {
+        if(i == 3) {
+            return "Fizz";
+        }
         return "0";
     }
 }
