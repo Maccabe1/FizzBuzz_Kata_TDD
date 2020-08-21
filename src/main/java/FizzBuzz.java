b63df0a6c3a50f5bdaccb3705cd71140916a45bf
Maccabe1 <gdmaccabe@hotmail.co.uk>
2020-08-21 23:33:24 +0300

    Add code to pass 5 equals Buzz test and add a modulator to check for correct numbers

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,8 +2,13 @@ package main.java;
 
 public class FizzBuzz {
     public static String returnOutput(int i) {
-        if(i == 3) {
+        if (i == 0) {
+            return "0";
+        }
+        if (i % 3 == 0) {
             return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
         }
         return "0";
     }
